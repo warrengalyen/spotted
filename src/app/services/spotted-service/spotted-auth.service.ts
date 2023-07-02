938b3dd3400fc05249aa7a16bdfe08bf98b55a75
Warren Galyen <wgalyen@hotmail.com>
2023-07-02 12:05:42 -0700

    feat(spotted-auth.service.ts): Mostly working authorization and updated logging service for better log granularity

--- src/app/services/spotted-service/spotted-auth.service.ts
+++ src/app/services/spotted-service/spotted-auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import { Http, Headers, Response } from '@angular/http';
 import { SpottedAppConstants } from "./spotted-service.config";
 import { SpottedCredentials } from './spotted-credentials';
+import { LoggingService, LOG_LEVEL } from '../logging-service/logging.service';
 
 /**
  * App authentication with Spotify
@@ -9,38 +10,61 @@ import { SpottedCredentials } from './spotted-credentials';
 @Injectable()
 export class SpottedAuthService {
 
-  constructor(private http: Http) {
+  scope = [
+    'user-read-email',
+    'user-read-currently-playing',
+    'user-modify-playback-state',
+    'streaming',
+    'user-read-playback-state',
+    'user-read-private',
+    'user-top-read',
+    'user-read-email'
+  ].join('%20');
+
+  constructor(private http: Http, private loggingService: LoggingService) {
 
   }
 
   /**
    * Allow the user to authorize itself when logging into Spotify
    */
-  public authorizeSpotify(): Promise<string> {
+  public authorizeSpotify(): void {
     const authUrl = SpottedAppConstants.API_ACCOUNT_URL + SpottedAppConstants.API_AUTH;
-    return this.http.get(authUrl, {
-      params: {
-        client_id: SpottedCredentials.client_id,
-        response_type: 'token',
-        redirect_uri: 'test'
-      }
-    })
-      .toPromise()
-      .then((response: Response) => {
-        if (response.status < 300) {
-          const data = response.json();
-          return data.redirect_url;
-        } else {
-          return false;
-        }
-      })
-      .catch(this.handleError);
+    window.location.href = this.buildUrlParam(authUrl + '?', {
+      client_id: SpottedCredentials.client_id,
+      response_type: 'token',
+      redirect_uri: 'localhost:4200',
+      scope: this.scope
+    });
   }
 
+  /* HELPER FUNCTIONS */
+  /**
+   * Helper function for building an url given a single level parameter object
+   * @param {string} url - the url to build on
+   * @param {*} params - object of parameters
+   * @returns {string} - returns the parameterized url
+   * @memberOf SpottedAuthService
+   */
+  private buildUrlParam(url: string, params: any): string {
+    Object.entries(params).forEach(
+      ([key, value]) => url += `&${ key }=${ value }`
+    );
+    return url;
+  }
+
+  /**
+   * Helper function for handling failure in Promise responses
+   *
+   * @private
+   * @param {*} error - the error payload
+   * @returns {Promise<any>} - return a resolution of the promise
+   * @memberof SpottedAuthService
+   */
   private handleError(error: any): Promise<any> {
     const errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    console.error(errMsg); // log to console
+    this.loggingService.log(errMsg, LOG_LEVEL.Severe); // log to console
     return Promise.resolve(false);
   }
 }
