1dbed375f196b02ce8546b212a7ac02ddc3c17d8
Warren Galyen <wgalyen@hotmail.com>
2023-07-06 15:01:06 -0700

    feat(song-card.component.css): Fixed issue with title being too long

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SpottedService } from './services/spotted-service/spotted.service';
-import { SpottedAuthService } from './services/spotted-service/spotted-auth.service';
-import { HttpModule } from '@angular/http';
-import { LoginComponent } from './pages/login/container/login/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardModule } from './layouts/dashboard/dashboard.module';
-import { AcceptComponent } from './pages/accept/accept.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {SpottedService} from './services/spotted-service/spotted.service';
+import {SpottedAuthService} from './services/spotted-service/spotted-auth.service';
+import {HttpModule} from '@angular/http';
+import {LoginComponent} from './pages/login/container/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardModule} from './layouts/dashboard/dashboard.module';
+import {AcceptComponent} from './pages/accept/accept.component';
+import {HttpClientModule} from '@angular/common/http';
 
 /* Font Awesome */
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
-import { HomeComponent } from './pages/dashboard/home/container/home.component';
-import { SongCardComponent } from './pages/dashboard/home/components/song-card/song-card.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faFeatherAlt} from '@fortawesome/free-solid-svg-icons';
+import {HomeComponent} from './pages/dashboard/home/container/home.component';
+import {SongCardComponent} from './pages/dashboard/home/components/song-card/song-card.component';
+import {ArtistCardComponent} from "./pages/dashboard/home/components/artist-card/artist-card.component";
 
 // Loading library icons
 library.add(faFeatherAlt);
@@ -28,6 +29,7 @@ library.add(faFeatherAlt);
     AcceptComponent,
     HomeComponent,
     SongCardComponent,
+    ArtistCardComponent,
   ],
   imports: [
     // Angular Specific
@@ -43,4 +45,5 @@ library.add(faFeatherAlt);
   providers: [SpottedService, SpottedAuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
