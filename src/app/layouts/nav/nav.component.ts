6ec397a6007802620f5b3c502588fc5da7edb6cb
Warren Galyen <wgalyen@hotmail.com>
2023-07-07 09:53:56 -0700

    feat(nav): Using routerlink with nav instead of href

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SpottedService } from "../../services/spotted-service/spotted.service";
 import {SpottedAuthService} from "../../services/spotted-service/spotted-auth.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,9 +10,25 @@ import {SpottedAuthService} from "../../services/spotted-service/spotted-auth.se
 })
 export class NavComponent implements OnInit {
 
-  constructor(private auth: SpottedAuthService) { }
+  public curUrl: string = '';
+
+  public pages: any = [
+    { name: 'Home', url: 'home' },
+    { name: 'Top Artists', url: 'top-artists' },
+    { name: 'Top Tracks', url: 'top-tracks' },
+    { name: 'Top Genres', url: '#' },
+    { name: 'Playlist Stats', url: '#' }
+  ];
+
+  constructor(private router: Router, private auth: SpottedAuthService) { }
 
   ngOnInit() {
+    this.curUrl = this.router.url.substring(this.router.url.indexOf('dashboard'));
+    console.log(this.curUrl);
+  }
+
+  private updateCurUrl() {
+    this.curUrl = this.router.url.substring(this.router.url.indexOf('dashboard'));
   }
 
   logout() {
