66ddb324c1a2acf12865566e3632f6f5a9af597f
Warren Galyen <wgalyen@hotmail.com>
2023-07-08 10:15:19 -0700

    feat(rankings): Started working on historical rankings, added ant-ui lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,41 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {SpottedService} from './services/spotted-service/spotted.service';
-import {SpottedAuthService} from './services/spotted-service/spotted-auth.service';
 import {HttpModule} from '@angular/http';
 import {LoginComponent} from './pages/login/container/login/login.component';
 import {AppRoutingModule} from './app-routing.module';
 import {DashboardModule} from './layouts/dashboard/dashboard.module';
 import {AcceptComponent} from './pages/accept/accept.component';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /* Font Awesome */
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
-import { faBars, faHeadphonesAlt, faMusic, faStar, faGenderless, faGripLines, faTh, faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHeadphonesAlt, faMusic, faStar, faGenderless, faGripLines, faTh, faChartLine, faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
 import { faEdit, faChartBar } from '@fortawesome/free-regular-svg-icons';
 
-// Loading library icons
-library.add(faBars, faEdit, faHeadphonesAlt, faMusic, faStar, faGenderless, faChartBar, faGripLines, faTh, faFeatherAlt);
+/* Ant Design Lib */
+// import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+
+/** config angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+import { SpottedService } from './services/spotted-service/spotted.service';
+import { SpottedAuthService } from './services/spotted-service/spotted-auth.service';
 import { HomeComponent } from './pages/dashboard/home/home.component';
 import { TopArtistsComponent } from './pages/dashboard/top-artists/top-artists.component';
 import { TopTracksComponent } from './pages/dashboard/top-tracks/top-tracks.component';
 import { ArtistTileComponent } from './pages/dashboard/components/artist-tile/artist-tile.component';
 import { SongTileComponent } from './pages/dashboard/components/song-tile/song-tile.component';
+import { RankingsComponent } from './pages/dashboard/rankings/rankings.component';
+
+registerLocaleData(en);
+
+// Loading library icons
+library.add(faBars, faEdit, faHeadphonesAlt, faMusic, faStar, faGenderless, faChartBar, faGripLines, faTh, faChartLine, faFeatherAlt);
 
 @NgModule({
   declarations: [
@@ -35,6 +48,7 @@ import { SongTileComponent } from './pages/dashboard/components/song-tile/song-t
     TopTracksComponent,
     ArtistTileComponent,
     SongTileComponent,
+    RankingsComponent,
   ],
   imports: [
     // Angular Specific
@@ -42,12 +56,13 @@ import { SongTileComponent } from './pages/dashboard/components/song-tile/song-t
     HttpModule,
     HttpClientModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
 
     // App Specific
     DashboardModule,
     FontAwesomeModule
   ],
-  providers: [SpottedService, SpottedAuthService],
+  providers: [SpottedService, SpottedAuthService, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
