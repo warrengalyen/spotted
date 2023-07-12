ce169f7f4e6ac958f8381033f2b0382c884654f5
Warren Galyen <wgalyen@hotmail.com>
2023-07-12 09:46:45 -0700

    feat(sidebar.component.scss): Started working on sidebar design

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'sidebar',
   styleUrls: ['./sidebar.component.scss'],
   template: `
-        <div>
-            <p>Sidebar</p>
-        </div>
-    `
+    <div class="sidebar md:flex md:flex-col md:justify-between md:min-h-screen p-4">
+      <div>
+        <div class="py-4">Logo</div>
+        <ul>
+          <li class="sidebar__link transition hover:font-bold">Home</li>
+          <li class="sidebar__link transition hover:font-bold">Top Artists</li>
+          <li class="sidebar__link transition hover:font-bold">Top Songs</li>
+          <li class="sidebar__link transition hover:font-bold">Top Genres</li>
+          <li class="sidebar__link transition hover:font-bold">Playlists</li>
+        </ul>
+      </div>
+      <div>Github Link</div>
+    </div>
+  `,
 })
 export class SidebarComponent {
   constructor() {}
-
 }
