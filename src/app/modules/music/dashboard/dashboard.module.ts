be771688503452bfd0bcb26ae2dfe9edb3c5ba68
Warren Galyen <wgalyen@hotmail.com>
2023-07-12 09:42:55 -0700

    feat(music.module.ts): Working login behavior and redirection, integrated AuthGuard

--- src/app/modules/music/dashboard/dashboard.module.ts
+++ src/app/modules/music/dashboard/dashboard.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
+import {RouterModule, Routes} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
+export const DASHBOARD_ROUTES: Routes = [{ path: '', component: DashboardComponent }];
 @NgModule({
-  imports: [],
+  imports: [CommonModule, RouterModule.forChild(DASHBOARD_ROUTES)],
   declarations: [DashboardComponent],
   providers: [],
 })
