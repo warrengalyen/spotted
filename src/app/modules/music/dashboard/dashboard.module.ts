3cb34d935aa2c2a85f41002cd6913172fa78083f
Warren Galyen <wgalyen@hotmail.com>
2023-07-12 16:37:50 -0700

    feat(dashboard-top-artists): Added dashboard view for top artists

--- src/app/modules/music/dashboard/dashboard.module.ts
+++ src/app/modules/music/dashboard/dashboard.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import {RouterModule, Routes} from '@angular/router';
 import {CommonModule} from '@angular/common';
+import { DashboardTopArtistComponent } from './components/dashboard-top-artist/dashboard-top-artist.component';
+import { DashboardTopArtistsComponent } from './components/dashboard-top-artists/dashboard-top-artists.component';
 
 export const DASHBOARD_ROUTES: Routes = [{ path: '', component: DashboardComponent }];
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(DASHBOARD_ROUTES)],
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, DashboardTopArtistComponent, DashboardTopArtistsComponent],
   providers: [],
 })
 export class DashboardModule {}
