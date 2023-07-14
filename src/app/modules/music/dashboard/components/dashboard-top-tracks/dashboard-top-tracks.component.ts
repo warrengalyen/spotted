d5ffaae2169fb2f3639d16c056f8893f11bfed6d
Warren Galyen <wgalyen@hotmail.com>
2023-07-14 10:14:16 -0700

    feat(navbar): Added navbar, updated UI

--- src/app/modules/music/dashboard/components/dashboard-top-tracks/dashboard-top-tracks.component.ts
+++ src/app/modules/music/dashboard/components/dashboard-top-tracks/dashboard-top-tracks.component.ts
@@ -7,7 +7,7 @@ import { TopTracksResponse } from '../../../shared/models/top.model';
   template: `
     <div class="mb-8">
       <div class="flex justify-between">
-        <p class="text-xl text-gray-500 font-semibold mb-4">
+        <p class="text-xl text-gray-500 font-semibold mb-4 tracking-tighter">
           Recent Top Tracks
         </p>
         <spotted-button text="See More"></spotted-button>
