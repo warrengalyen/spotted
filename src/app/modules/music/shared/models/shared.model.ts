66b13274fe586036f0738da0393353befd55a407
Warren Galyen <wgalyen@hotmail.com>
2023-07-16 09:10:47 -0700

    feat(playlists.service.ts): Working Playlists service

--- src/app/modules/music/shared/models/shared.model.ts
+++ src/app/modules/music/shared/models/shared.model.ts
@@ -5,3 +5,44 @@ export interface Image {
   url: string;
   width: string;
 }
+
+export interface Followers {
+  href: string;
+  followers: number;
+}
+
+
+export interface Artist {
+  external_urls: StringObject;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+
+export interface Album {
+  images: Image[];
+  name: string;
+  [x: string]: any;
+}
+
+export interface Track {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: StringObject;
+  external_urls: StringObject;
+  href: string;
+  id: string;
+  is_local: boolean;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
