25feb14737f13a6f197590902bfa886043ba9016
Warren Galyen <wgalyen@hotmail.com>
2023-07-18 16:00:47 -0700

    feat(artists.service.ts): Fixed UI for tablets for playlist view, added ArtistService

--- src/app/modules/music/shared/shared.module.ts
+++ src/app/modules/music/shared/shared.module.ts
@@ -4,8 +4,8 @@ import {NavBarComponent} from 'src/app/components/navbar/navbar.component';
 import {ProfileButton} from 'src/app/components/profile-button/profile-button.component';
 import {DashboardTopArtistComponent} from '../dashboard/components/dashboard-top-artist/dashboard-top-artist.component';
 import {TopTracksItemComponent} from '../top-tracks/components/top-tracks-item/top-tracks-item.component';
+import { TimeRangeComponent } from './components/time-range/time-range.component';
 import {ProfileService} from './services/profile.service';
-import {TimeRangeComponent} from './components/time-range/time-range.component';
 
 @NgModule({
   imports: [CommonModule],
@@ -25,5 +25,4 @@ import {TimeRangeComponent} from './components/time-range/time-range.component';
     TopTracksItemComponent,
   ],
 })
-export class MusicSharedModule {
-}
+export class MusicSharedModule {}
