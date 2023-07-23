b817551f59bb6cd7129ca67c0a7b9b06bf83b73a
Warren Galyen <wgalyen@hotmail.com>
2023-07-23 12:03:18 -0700

    feat(track-details.component.ts): Added pulser for tempo, added ng-content to track-card

--- src/app/modules/music/track/components/track-card/track-card.component.ts
+++ src/app/modules/music/track/components/track-card/track-card.component.ts
@@ -4,14 +4,22 @@ import { Component, Input } from '@angular/core';
   selector: 'track-card',
   styleUrls: ['./track-card.component.scss'],
   template: `
-        <div class="rounded-lg bg-gray-900 px-8 py-4 cursor-pointer duration-200 transition-all hover:bg-gray-800">
-            <p class="uppercase font-light tracking-widest text-gray-500 text-sm">{{ title }}</p>
-            <p class="capitalize font-bold text-2xl">{{ text }}</p>
+    <div
+      class="rounded-lg bg-gray-900 px-8 py-4 cursor-pointer duration-200 transition-all hover:bg-gray-800"
+    >
+      <div class="flex items-center">
+        <div>
+          <p class="uppercase font-light tracking-widest text-gray-500 text-sm">
+            {{ title }}
+          </p>
+          <p class="capitalize font-bold text-2xl">{{ text }}</p>
         </div>
-    `
+        <ng-content></ng-content>
+      </div>
+    </div>
+  `,
 })
 export class TrackCardComponent {
-
   @Input() title: string;
   @Input() text: string;
 
